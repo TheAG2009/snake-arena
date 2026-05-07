7e3e0f77fd170d052e246d9844e38ee70e93f53b
TheAG2009 <adityaghodke2009@gmail.com>
2026-05-07 11:49:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
-const CACHE = 'ag-snake-v2';
-const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
+const CACHE_NAME = 'ag-snake-v2.4';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './assets/sounds/eat.wav',
+    './assets/sounds/hit.wav',
+    './assets/sounds/bgm.mp3'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(assets)));
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
